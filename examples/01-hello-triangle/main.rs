ab9a437454cc818e1d3dfc7cf2d06756806f7306
Jason Luo <luojason1227@gmail.com>
2024-09-22 01:22:20 -0700

    Move binary into examples folder

--- examples/01-hello-triangle/main.rs
+++ examples/01-hello-triangle/main.rs
@@ -1,7 +1,23 @@
-//! This module contains the implementation of most of the initialization,
-//! rendering and display logic used during runtime.
-
 use gltut::glutil::{GlProgram, GlShader, GlShaderType};
+
+use anyhow::Context;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // SAFETY: do not drop _window
+    let (event_loop, _window, gl_context, surface) = unsafe { gltut::init_window_and_context()? };
+    let triangles = TriangleExample::new();
+
+    let mut app = gltut::app::GlAppBuilder::new()
+        .with_display(|| triangles.display())
+        .build(gl_context, surface);
+
+    // run event loop
+    event_loop
+        .run_app(&mut app)
+        .context("failed to start event_loop")?;
+
+    Ok(())
+}
 
 /// Positions of the triangle vertices in homogeneous coordinates.
 #[rustfmt::skip]
