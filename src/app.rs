ba2b1b71d704ca58b7bd77b36214b175f2b648e1
Jason Luo <luojason1227@gmail.com>
2024-09-27 23:44:11 -0700

    refactor: Change GlApp to use delegate pattern

--- src/app.rs
+++ src/app.rs
@@ -11,10 +11,26 @@ use winit::dpi::PhysicalSize;
 use winit::window::Window;
 use winit::{application::ApplicationHandler, event::WindowEvent};
 
-/// A helper struct for building a [`GlApp`].
-pub struct GlAppBuilder<T1, T2> {
-    display_fn: T1,
-    reshape_fn: T2,
+/// A trait specifying methods required by [`GlApp`] for running a window application.
+///
+/// Default implementations are provided for all methods, which generally do nothing and result in a static window.
+pub trait GlAppDelegate {
+    /// Called when a window redraw is requested, for doing any rendering/updates needed.
+    #[allow(unused_variables)]
+    fn display(&mut self, app: &GlAppContext) {
+        // provided implementation: do nothing
+    }
+
+    /// Called when the window has been resized, for adjusting the OpenGL viewport and the like.
+    ///
+    /// The new window size is provided with type [`PhysicalSize`],
+    /// since `glViewPort` expects window coordinates to be specified in physical pixels.
+    /// It can be converted to [`LogicalSize`](winit::dpi::LogicalSize) if needed
+    /// by accessing the window provided by the [`GlAppContext`] handle.
+    #[allow(unused_variables)]
+    fn reshape(&mut self, app: &GlAppContext, size: &PhysicalSize<u32>) {
+        set_gl_viewport(size);
+    }
 }
 
 fn do_nothing() {}
@@ -28,73 +44,43 @@ fn set_gl_viewport(size: &PhysicalSize<u32>) {
     }
 }
 
-impl GlAppBuilder<(), ()> {
-    /// Initialize builder with default callbacks.
-    ///
-    /// Generally the default callbacks do nothing.
-    pub fn new() -> GlAppBuilder<impl FnMut(), impl FnMut(&PhysicalSize<u32>)> {
-        GlAppBuilder {
-            display_fn: do_nothing,
-            reshape_fn: set_gl_viewport,
-        }
-    }
+/// Allows [`GlAppDelegate`] to access handles to the various window and OpenGL related structs.
+pub struct GlAppContext {
+    pub window: Window,
+    pub context: PossiblyCurrentContext,
+    pub surface: Surface<WindowSurface>,
+    _private: (), // prevent external modules from instantiating this struct
 }
 
-impl<T1, T2> GlAppBuilder<T1, T2> {
-    /// Set a custom `display` callback. See [`GlApp`] for details.
-    pub fn with_display<F: FnMut()>(self, display: F) -> GlAppBuilder<F, T2> {
-        GlAppBuilder {
-            display_fn: display,
-            reshape_fn: self.reshape_fn,
-        }
-    }
+/// An basic implementation of [`ApplicationHandler`].
+///
+/// It uses a limited set of handlers to respond to some standard scenarios.
+///
+/// Construct a [`GlApp`] by providing an implementation of [`GlAppDelegate`],
+/// or by using [`GlAppBuilder::new()`] if the callbacks are simple functions.
+pub struct GlApp<T> {
+    delegate: T,
+    app: GlAppContext,
+}
 
-    /// Set a custom `reshape` callback. See [`GlApp`] for details.
-    pub fn with_reshape<F: FnMut(&PhysicalSize<u32>)>(self, reshape: F) -> GlAppBuilder<T1, F> {
-        GlAppBuilder {
-            display_fn: self.display_fn,
-            reshape_fn: reshape,
-        }
-    }
-
-    /// Build the [`GlApp`].
-    pub fn build(
-        self,
+impl<T: GlAppDelegate> GlApp<T> {
+    pub fn new(
+        delegate: T,
         window: Window,
         context: PossiblyCurrentContext,
         surface: Surface<WindowSurface>,
-    ) -> GlApp<T1, T2> {
-        GlApp {
-            display_fn: self.display_fn,
-            reshape_fn: self.reshape_fn,
+    ) -> Self {
+        let app = GlAppContext {
             window,
             context,
             surface,
-        }
+            _private: (),
+        };
+        Self { delegate, app }
     }
 }
 
-/// An basic implementation of [`ApplicationHandler`]
-/// which allows configuration of a limited set of callbacks to handle some standard scenarios.
-///
-/// The callbacks are:
-///  - `display`: Called when a window redraw is requested, for doing any rendering/updates needed.
-///  - `reshape`: Called when the window has been resized, for adjusting the OpenGL viewport and the like.
-///
-/// Construct a `GlApp` using [`GlAppBuilder::new()`].
-pub struct GlApp<T1, T2> {
-    display_fn: T1,
-    reshape_fn: T2,
-    window: Window,
-    context: PossiblyCurrentContext,
-    surface: Surface<WindowSurface>,
-}
-
-impl<T1, T2> ApplicationHandler for GlApp<T1, T2>
-where
-    T1: FnMut(),
-    T2: FnMut(&PhysicalSize<u32>),
-{
+impl<T: GlAppDelegate> ApplicationHandler for GlApp<T> {
     fn resumed(&mut self, _: &winit::event_loop::ActiveEventLoop) {}
 
     fn window_event(
@@ -108,23 +94,96 @@ where
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::RedrawRequested => {
                 // call user-specified display function
-                (self.display_fn)();
+                self.delegate.display(&self.app);
 
                 // render the results
                 unsafe {
                     gl::Flush();
                 }
-                self.window.pre_present_notify();
+                self.app.window.pre_present_notify();
                 // NOTE: swap buffers is important, can get get doubled buffered surface even when single buffered is requested
-                self.surface
-                    .swap_buffers(&self.context)
+                self.app
+                    .surface
+                    .swap_buffers(&self.app.context)
                     .expect("failed to swap GLSurface buffers");
             }
             WindowEvent::Resized(size) => {
-                self.window.resize_surface(&self.surface, &self.context);
-                (self.reshape_fn)(&size);
+                self.app
+                    .window
+                    .resize_surface(&self.app.surface, &self.app.context);
+                self.delegate.reshape(&self.app, &size);
             }
             _ => (),
         };
+    }
+}
+
+/// A helper struct for building a basic [`GlApp`].
+///
+/// Can be used to pass simple callbacks for each of the methods in [`GlAppDelegate`].
+/// If the callbacks need access to [`GlAppContext`] (e.g. to access the window) or if more control is needed,
+/// prefer to directly implement [`GlAppDelegate`].
+pub struct GlAppBuilder<T1, T2> {
+    display_fn: T1,
+    reshape_fn: T2,
+}
+
+impl GlAppBuilder<(), ()> {
+    /// Initialize builder with default callbacks.
+    ///
+    /// The default callbacks have the same behavior as the default implementations in [`GlAppDelegate`].
+    pub fn new() -> GlAppBuilder<impl FnMut(), impl FnMut(&PhysicalSize<u32>)> {
+        GlAppBuilder {
+            display_fn: do_nothing,
+            reshape_fn: set_gl_viewport,
+        }
+    }
+}
+
+impl<T1, T2> GlAppBuilder<T1, T2> {
+    /// Set a custom `display` callback. See [`GlAppDelegate`] for details.
+    pub fn with_display<F: FnMut()>(self, display: F) -> GlAppBuilder<F, T2> {
+        GlAppBuilder {
+            display_fn: display,
+            reshape_fn: self.reshape_fn,
+        }
+    }
+
+    /// Set a custom `reshape` callback. See [`GlAppDelegate`] for details.
+    pub fn with_reshape<F: FnMut(&PhysicalSize<u32>)>(self, reshape: F) -> GlAppBuilder<T1, F> {
+        GlAppBuilder {
+            display_fn: self.display_fn,
+            reshape_fn: reshape,
+        }
+    }
+}
+
+impl<T1, T2> GlAppBuilder<T1, T2>
+where
+    T1: FnMut(),
+    T2: FnMut(&PhysicalSize<u32>),
+{
+    /// Build the [`GlApp`].
+    pub fn build(
+        self,
+        window: Window,
+        context: PossiblyCurrentContext,
+        surface: Surface<WindowSurface>,
+    ) -> GlApp<Self> {
+        GlApp::new(self, window, context, surface)
+    }
+}
+
+impl<T1, T2> GlAppDelegate for GlAppBuilder<T1, T2>
+where
+    T1: FnMut(),
+    T2: FnMut(&PhysicalSize<u32>),
+{
+    fn display(&mut self, _: &GlAppContext) {
+        (self.display_fn)()
+    }
+
+    fn reshape(&mut self, _: &GlAppContext, size: &PhysicalSize<u32>) {
+        (self.reshape_fn)(size)
     }
 }
